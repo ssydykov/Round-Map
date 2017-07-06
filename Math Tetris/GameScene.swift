d3de0f5b40a58f5886574df9e98eb39650a58682
ssydykov <saken2316@gmail.com>
2017-07-06 16:19:15 -0700

    1) Added key item, 2) Teleports, 3) Obstacles, 4) Door open only when got a key

--- Math Tetris/GameScene.swift
+++ Math Tetris/GameScene.swift
@@ -14,17 +14,29 @@ var currentLevel: Int = 1
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    // Nodes and Buttons
+    // Nodes
     var leftButton: SKSpriteNode!
     var rightButton: SKSpriteNode!
     var map: SKSpriteNode!
     var endDialog: SKSpriteNode!
     var circle: SKSpriteNode!
+    var key: SKSpriteNode?
+    var keySprite: SKSpriteNode!
+    var teleport1: SKSpriteNode?
+    var teleport2: SKSpriteNode?
+    
+    // Buttons
     var nextLevelButton: MSButtonNode!
+    var menuButton: MSButtonNode!
+    var restartButton: MSButtonNode!
+    
+    // UI Labels
     var scoreLabel: SKLabelNode!
+    var exitLabel: SKLabelNode!
     
     // Booleans
     var onTouch: Bool = false
+    var onTeleport: Bool = true
     
     // Variables
     var score: Int = 0
@@ -35,6 +47,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.contactDelegate = self
         
         // Initiate variables
+        
         circle = childNode(withName: "//circle") as! SKSpriteNode
         leftButton = childNode(withName: "leftButton") as! SKSpriteNode
         rightButton = childNode(withName: "rightButton") as! SKSpriteNode
@@ -42,9 +55,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         endDialog = childNode(withName: "//endDialog") as! SKSpriteNode
         endDialog.zPosition = -5
         nextLevelButton = childNode(withName: "//nextLevelButton") as! MSButtonNode
+        menuButton = childNode(withName: "//menuButton") as! MSButtonNode
         scoreLabel = childNode(withName: "//score") as! SKLabelNode
+        key = childNode(withName: "//key") as? SKSpriteNode
+        keySprite = childNode(withName: "//keySprite") as! SKSpriteNode
+        exitLabel = childNode(withName: "//exitLabel") as! SKLabelNode
+        restartButton = childNode(withName: "//restartButton") as! MSButtonNode
+        teleport1 = childNode(withName: "//teleport1") as? SKSpriteNode
+        teleport2 = childNode(withName: "//teleport2") as? SKSpriteNode
         
-        // createCircle(160.0, 225.0)
+        // Check is it key in the scene
+        
+        if (key == nil){
+            
+            print("key node is not in the scene")
+            
+        } else {
+            
+            print("key node is in the scene")
+            
+            // Set exit label to closed
+            exitLabel.text = "Closed"
+            exitLabel.fontSize = 13
+        }
         
         // Next level button clicked
         nextLevelButton.selectedHandler = {
@@ -52,14 +85,43 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             print("Start next level")
         
             currentLevel += 1
-            guard let scene = GameScene.loadLevel(currentLevel ) else {
+            guard let scene = GameScene.loadLevel(currentLevel) else {
 
                 print ("Level is missing?")
                 return
             }
             
             view.presentScene(scene)
+        }
+        
+        // Restart button clicked
+        restartButton.selectedHandler = {
+            
+            print ("Restart button clicked")
+            
+            guard let scene = GameScene.loadLevel(currentLevel) else {
+                
+                print ("Level is missing?")
+                return
             }
+            
+            view.presentScene(scene)
+        }
+        
+        // Menu button clicked
+        menuButton.selectedHandler = {
+            
+            print("Menu button clicked")
+            
+            guard let scene = GameScene(fileNamed: "Menu") else {
+                
+                print("Level is missing?")
+                return
+            }
+            
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
+        }
     }
     
     // Calls when touch begins
@@ -112,7 +174,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let nodeB = contactB.node!
         
         // If circle collides with exit
-        if nodeA.name == "exit" || nodeB.name == "exit" {
+        if ((nodeA.name == "exit" || nodeB.name == "exit") &&
+            exitLabel.text != "Closed"){
             
             print("Exit is collide")
 
@@ -126,8 +189,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             circle.removeFromParent()
             
             return
-            
-        } else if nodeA.name == "obstacle" || nodeB.name == "obstacle" {
+        }
+
+        // If circle collides with obstale
+
+        else if nodeA.name == "obstacle" || nodeB.name == "obstacle" {
             
             print("Hits obstacle")
             
@@ -138,7 +204,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             }
             
             self.view?.presentScene(scene)
-        } else if nodeA.name == "star" || nodeB.name == "star" {
+        }
+        
+        // If circle collides with star
+        
+        else if nodeA.name == "star" || nodeB.name == "star" {
             
             score += 1
             scoreLabel.text = ": \(score)"
@@ -148,6 +218,81 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             } else {
                 nodeB.removeFromParent()
             }
+        }
+        
+        // If circle collides with key
+        
+        else if nodeA.name == "key" || nodeB.name == "key" {
+            
+            keySprite.isHidden = false
+            exitLabel.text = "Open"
+            exitLabel.fontSize = 16
+            
+            if nodeA.name == "key" {
+                nodeA.removeFromParent()
+            } else {
+                nodeB.removeFromParent()
+            }
+        }
+        
+        // If circle collides with teleports
+        
+        else if nodeA.name == "teleport1" || nodeB.name == "teleport1" {
+            
+            if (teleport2 != nil && onTeleport) {
+                
+                onTeleport = false
+                
+                print("circle position = \(circle.position), teleport1 position = \(teleport1!.position), teleport2 position = \(teleport2!.position)")
+                
+                let moveCircle = SKAction.move(to: teleport2!.position, duration: 0)
+                circle.run(moveCircle)
+            
+            } else {
+                
+                print("Teleport is nil")
+            
+            }
+        }
+        
+        else if nodeA.name == "teleport2" || nodeB.name == "teleport2" {
+            
+            if (teleport1 != nil && onTeleport) {
+                
+                onTeleport = false
+                
+                print("circle position = \(circle.position), teleport1 position = \(teleport1!.position), teleport2 position = \(teleport2!.position)")
+                
+                let moveCircle = SKAction.move(to: teleport1!.position, duration: 0)
+                circle.run(moveCircle)
+            
+            } else {
+                
+                print("Teleport is nil")
+            
+            }
+        }
+    }
+    
+    // Some two objects stop colliding
+    func didEnd(_ contact: SKPhysicsContact) {
+        
+        print("Something stop colliding")
+        
+        /* Get references to bodies involved in collision */
+        let contactA = contact.bodyA
+        let contactB = contact.bodyB
+        
+        /* Get references to the physics body parent nodes */
+        let nodeA = contactA.node!
+        let nodeB = contactB.node!
+        
+        // If circle stop colliding with teleports
+        
+        if nodeA.name == "teleport1" || nodeB.name == "teleport1" ||
+            nodeA.name == "teleport2" || nodeB.name == "teleport2" {
+            
+            onTeleport = true
         }
     }
     
