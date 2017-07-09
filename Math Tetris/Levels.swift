083d1b9184f0c3f5885bf5fdb3818384eaef23c9
ssydykov <saken2316@gmail.com>
2017-07-09 01:58:30 -0700

    1) Add items for levels in Levels scene, 2) Added logic for right and left arrow button, 3) Camera move right when right button is clicked and move left when left button is clicked

--- Math Tetris/Levels.swift
+++ Math Tetris/Levels.swift
@@ -12,28 +12,79 @@ import GameplayKit
 class Levels: SKScene {
     
     // UI variable
+    var rightButton: MSButtonNode!
+    var leftButton: MSButtonNode!
+    var cameraNode: SKCameraNode!
     
     // Variables
     var levels: Array<Level> = []
+    let numberOfLevels = 11
     
     override func didMove(to view: SKView) {
         
-        let level1 = Level(1, "Level 1", true, 0, "0")
-        levels.append(level1)
+        // Initialize variables
+        rightButton = self.childNode(withName: "//rightButton") as! MSButtonNode
+        leftButton = self.childNode(withName: "//leftButton") as! MSButtonNode
+        cameraNode = self.childNode(withName: "camera") as! SKCameraNode
+        self.camera = cameraNode
         
-        let levelsData = NSKeyedArchiver.archivedData(withRootObject: levels)
-        UserDefaults.standard.set(levelsData, forKey: "levels")
         
-        UserDefaults.standard.synchronize()
+        // Right arrow button is clicked
+        rightButton.selectedHandler = {
+            
+            print("Right arrow clicked")
+            self.cameraNode.position.x += 568
+        }
         
-        print("Level is in store")
+        // Left arrow button is clicked
+        leftButton.selectedHandler = {
+            
+            print("Left arrow clicked")
+            self.cameraNode.position.x -= 568
+        }
         
+        
+        // If there are stored data with levels
         if let data = UserDefaults.standard.object(forKey: "levels") as? Data {
             if let storedData = NSKeyedUnarchiver.unarchiveObject(with: data) as? [Level] {
-                // In here you can access your array
                 
-                print("Levels \(storedData[0].label)") 
+                levels = storedData
+                
+                print("There are some list of levels")
             }
+        }
+        
+        // If game was opened first time
+        else {
+            
+            for item in 1...numberOfLevels {
+                
+                // Set first level is opened by default
+                let level1 = Level(0, "Level 1", true, 0, "0")
+                levels.append(level1)
+                
+                // Set other levels as closed
+                levels.append(Level(item, "Level \(item + 1)", false, 0, "0"))
+            }
+            
+            // Save levels in data
+            let levelsData = NSKeyedArchiver.archivedData(withRootObject: levels)
+            UserDefaults.standard.set(levelsData, forKey: "levels")
+            UserDefaults.standard.synchronize()
+            
+            print("Levels are saved")
+        }
+        
+        // Show list of levels
+        showList()
+    }
+    
+    func showList(){
+        
+        for item in 0...numberOfLevels {
+            
+            let listItem = self.childNode(withName: "//Level \(item)") as! SKShapeNode
+            print(listItem.fillColor)
         }
     }
 }
